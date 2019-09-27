10c163b43bf598b2e4cb41098dcd908182572075
Stephen Roberts <stephen.roberts@skybettingandgaming.com>
2019-09-27 15:35:07 +0100

    PUT method to update employee records

--- src/main/kotlin/com/example/serversidekotlin/Model.kt
+++ src/main/kotlin/com/example/serversidekotlin/Model.kt
@@ -1,7 +1,11 @@
 package com.example.serversidekotlin
 
-data class Employee(val id: Int,
-                    val name: String,
-                    val age: Int,
-                    val dept: String,
-                    val salary: Double)
+data class Employee(
+    val id: Int,
+    val name: String,
+    val age: Int,
+    var dept: String,
+    var salary: Double
+)
+
+data class EmployeeUpdateReq(val dept: String?, val salary: Double?)
